bccb9d3d1a3d8a8bf26c34e7c9585c3dacff2526
Jonathan <jonathan@carter.games>
2022-04-26 08:05:57 +0100

    Create ObjectPool.cs
    - Added the work in progress object pool script from C.I.W.S to this as it was kinda good xD

---
+++ General/Pooling/ObjectPool.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adriana
+{
+    public class ObjectPool<T>
+    {
+        private readonly List<T> memberObjects;
+        private readonly HashSet<T> unavailableObjects;
+        private readonly GameObject prefab;
+        private readonly Transform parent;
+        
+        
+        public ObjectPool(GameObject prefab, Transform parent, int initialCount)
+        {
+            this.prefab = prefab;
+            this.parent = parent;
+
+            memberObjects = new List<T>();
+            unavailableObjects = new HashSet<T>();
+
+            for (var i = 0; i < initialCount; i++)
+                memberObjects.Add(Object.Instantiate(prefab, parent).GetComponentInChildren<T>(true));
+        }
+
+
+        private T Create()
+        {
+            var newMember = Object.Instantiate(prefab, parent).GetComponentInChildren<T>(true);
+            memberObjects.Add(newMember);
+            return newMember;
+        }
+        
+
+        public T Assign()
+        {
+            foreach (var t in memberObjects)
+            {
+                if (unavailableObjects.Contains(t)) continue;
+                unavailableObjects.Add(t);
+                return t;
+            }
+
+            var newMember = Create();
+            unavailableObjects.Add(newMember);
+            return newMember;
+        }
+
+
+        public void Return(T member)
+        {
+            unavailableObjects.Remove(member);
+        }
+    }
+}
